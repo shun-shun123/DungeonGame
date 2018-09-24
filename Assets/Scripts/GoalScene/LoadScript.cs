fcef2dd75d67e3ca7350e0d8582f25493bbe6335
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-25 00:06:18 +0900

    Goalシーンを作成してクリアタイムを表示するようにした

---
+++ Assets/Scripts/GoalScene/LoadScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScript : MonoBehaviour {
+    private int clearTime = 0;
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        clearTime = UnityChanMovementScript.GetTimer();
+        text.text += clearTime.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MainScene/UnityChanMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnityChanMovementScript : MonoBehaviour {
+    private static float timeCounter = 0.0f;
+    private Animator animator;
+    private float speed = 3.0f;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            animator.SetBool("is_running", true);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        } else {
+            animator.SetBool("is_running", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Rotate(new Vector3(0, 1, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Rotate(new Vector3(0, -1, 0));
+        }
+        // Goal時に渡すタイマーカウント
+        timeCounter += 1.0f * Time.deltaTime;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal")) {
+            SceneManager.LoadScene("Goal");
+        }
+    }
+
+    public static int GetTimer() {
+        return (int)timeCounter;
+    }
+}
