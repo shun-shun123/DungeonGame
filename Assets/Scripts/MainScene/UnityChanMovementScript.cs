00ccc133b49768de3ba985df0c796a843767af70
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-26 15:33:32 +0900

    ジョイスティックを追加した

--- Assets/Scripts/MainScene/UnityChanMovementScript.cs
+++ Assets/Scripts/MainScene/UnityChanMovementScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class UnityChanMovementScript : MonoBehaviour {
     private static float timeCounter = 0.0f;
@@ -14,21 +15,47 @@ public class UnityChanMovementScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            animator.SetBool("is_running", true);
-            transform.position += transform.forward * speed * Time.deltaTime;
-        } else {
-            animator.SetBool("is_running", false);
-        }
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            transform.Rotate(new Vector3(0, 1, 0));
-        }
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            transform.Rotate(new Vector3(0, -1, 0));
-        }
+        LaptopPlatformMove();
+        SmartphonePlatformMove();
         // Goal時に渡すタイマーカウント
         timeCounter += 1.0f * Time.deltaTime;
 	}
+
+    private void LaptopPlatformMove() {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool("is_running", true);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else
+        {
+            animator.SetBool("is_running", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -1, 0));
+        }
+    }
+
+    private void SmartphonePlatformMove() {
+        float y = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        float x = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        if (y.Equals(0.0f) && x.Equals(0.0f))
+        {
+            animator.SetBool("is_running", false);
+            return;
+        }
+        else if (!y.Equals(0.0f))
+        {
+            animator.SetBool("is_running", true);
+        }
+        transform.position += transform.forward * y * speed * Time.deltaTime;
+        transform.rotation = new Quaternion(0, 1, 0, x * 180);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
